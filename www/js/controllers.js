043b8a4ebf87b20e9aaccac0c568cc6287186537
ankush1990 <ank_ankush@yahoo.co.in>
2016-12-17 17:13:23 +0530

    201612171713

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -41,121 +41,47 @@ angular.module('starter.controllers', [])
   };
 })
 
-.controller('SearchCtrl', function ($scope, $cordovaCamera, $cordovaFile, $cordovaFileTransfer, $cordovaDevice, $ionicPopup, $cordovaActionSheet) {
-  	$scope.image = null;
- 
-	$scope.showAlert = function(title, msg) {
-		var alertPopup = $ionicPopup.alert({
-		  title: title,
-		  template: msg
+.controller('SearchCtrl', function ($scope, $cordovaCamera) {
+  // open PhotoLibrary
+    $scope.takePhoto = function () {
+		var options = {
+			quality: 75,
+			destinationType: Camera.DestinationType.DATA_URL,
+			sourceType: Camera.PictureSourceType.CAMERA,
+			allowEdit: true,
+			encodingType: Camera.EncodingType.JPEG,
+			targetWidth: 300,
+			targetHeight: 300,
+			popoverOptions: CameraPopoverOptions,
+			saveToPhotoAlbum: false
+		};
+		
+		$cordovaCamera.getPicture(options).then(function (imageData) {
+			$scope.imgURI = "data:image/jpeg;base64," + imageData;
+		}, function (err) {
+			// An error occured. Show a message to the user
 		});
-	};
-	
-	// Present Actionsheet for switch beteen Camera / Library
-	$scope.loadImage = function() {
-	  var options = {
-		title: 'Select Image Source',
-		buttonLabels: ['Load from Library', 'Use Camera'],
-		addCancelButtonWithLabel: 'Cancel',
-		androidEnableCancelButton : true,
-	  };
-	  $cordovaActionSheet.show(options).then(function(btnIndex) {
-		var type = null;
-		if (btnIndex === 1) {
-		  type = Camera.PictureSourceType.PHOTOLIBRARY;
-		} else if (btnIndex === 2) {
-		  type = Camera.PictureSourceType.CAMERA;
 		}
-		if (type !== null) {
-		  $scope.selectPicture(type);
+		
+		$scope.choosePhoto = function () {
+		var options = {
+			quality: 75,
+			destinationType: Camera.DestinationType.DATA_URL,
+			sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+			allowEdit: true,
+			encodingType: Camera.EncodingType.JPEG,
+			targetWidth: 300,
+			targetHeight: 300,
+			popoverOptions: CameraPopoverOptions,
+			saveToPhotoAlbum: false
+		};
+		
+		$cordovaCamera.getPicture(options).then(function (imageData) {
+				$scope.imgURI = "data:image/jpeg;base64," + imageData;
+			}, function (err) {
+				// An error occured. Show a message to the user
+			});
 		}
-	  });
-	};
-	
-	// Take image with the camera or from library and store it inside the app folder
-	// Image will not be saved to users Library.
-	$scope.selectPicture = function(sourceType) {
-	  var options = {
-		quality: 100,
-		destinationType: Camera.DestinationType.FILE_URI,
-		sourceType: sourceType,
-		saveToPhotoAlbum: false
-	  };
-	 
-	  $cordovaCamera.getPicture(options).then(function(imagePath) {
-		// Grab the file name of the photo in the temporary directory
-		var currentName = imagePath.replace(/^.*[\\\/]/, '');
-	 
-		//Create a new name for the photo
-		var d = new Date(),
-		n = d.getTime(),
-		newFileName =  n + ".jpg";
-	 
-		// If you are trying to load image from the gallery on Android we need special treatment!
-		if ($cordovaDevice.getPlatform() == 'Android' && sourceType === Camera.PictureSourceType.PHOTOLIBRARY) {
-		  window.FilePath.resolveNativePath(imagePath, function(entry) {
-			window.resolveLocalFileSystemURL(entry, success, fail);
-			function fail(e) {
-			  console.error('Error: ', e);
-			}
-	 
-			function success(fileEntry) {
-			  var namePath = fileEntry.nativeURL.substr(0, fileEntry.nativeURL.lastIndexOf('/') + 1);
-			  // Only copy because of access rights
-			  $cordovaFile.copyFile(namePath, fileEntry.name, cordova.file.dataDirectory, newFileName).then(function(success){
-				$scope.image = newFileName;
-			  }, function(error){
-				$scope.showAlert('Error', error.exception);
-			  });
-			};
-		  }
-		);
-		} else {
-		  var namePath = imagePath.substr(0, imagePath.lastIndexOf('/') + 1);
-		  // Move the file to permanent storage
-		  $cordovaFile.moveFile(namePath, currentName, cordova.file.dataDirectory, newFileName).then(function(success){
-			$scope.image = newFileName;
-		  }, function(error){
-			$scope.showAlert('Error', error.exception);
-		  });
-		}
-	  },
-	  function(err){
-		// Not always an error, maybe cancel was pressed...
-	  })
-	};
-	
-	// Returns the local path inside the app for an image
-	$scope.pathForImage = function(image) {
-	  if (image === null) {
-		return '';
-	  } else {
-		return cordova.file.dataDirectory + image;
-	  }
-	};
-	
-	$scope.uploadImage = function() {
-	  // Destination URL
-	  var url = "http://localhost:8888/upload.php";
-	 
-	  // File for Upload
-	  var targetPath = $scope.pathForImage($scope.image);
-	 
-	  // File name only
-	  var filename = $scope.image;;
-	 
-	  var options = {
-		fileKey: "file",
-		fileName: filename,
-		chunkedMode: false,
-		mimeType: "multipart/form-data",
-		params : {'fileName': filename}
-	  };
-	 
-	  $cordovaFileTransfer.upload(url, targetPath, options).then(function(result) {
-		$scope.showAlert('Success', 'Image upload finished.');
-	  });
-	}
 })
 
 .controller('PlaylistsCtrl', function($scope) {
