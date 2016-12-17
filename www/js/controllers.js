3be0b7225caefe480f9ca6be2ee388a4f62347c3
ankush1990 <ank_ankush@yahoo.co.in>
2016-12-17 16:20:32 +0530

    201612171620

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -41,57 +41,47 @@ angular.module('starter.controllers', [])
   };
 })
 
-.controller('SearchCtrl', function ($scope, $cordovaCamera, $cordovaFile, $cordovaFileTransfer, $ionicPopup, $cordovaActionSheet) {
+.controller('SearchCtrl', function ($scope, $cordovaCamera) {
   // open PhotoLibrary
-    $scope.openPhotoLibrary = function() {
-        var options = {
-            quality: 50,
-            destinationType: Camera.DestinationType.FILE_URI,
-            sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
-            allowEdit: true,
-            encodingType: Camera.EncodingType.JPEG,
-            popoverOptions: CameraPopoverOptions,
-            saveToPhotoAlbum: false
-        };
-
-        $cordovaCamera.getPicture(options).then(function(imageData) {
-
-            //console.log(imageData);
-            //console.log(options);   
-            var image = document.getElementById('tempImage');
-            image.src = imageData;  
-
-            var server = "http://yourdomain.com/upload.php",
-                filePath = imageData;
-
-            var date = new Date();
-
-            var options = {
-                fileKey: "file",
-                fileName: imageData.substr(imageData.lastIndexOf('/') + 1),
-                chunkedMode: false,
-                mimeType: "image/jpg"
-            };
-
-            $cordovaFileTransfer.upload(server, filePath, options).then(function(result) {
-                console.log("SUCCESS: " + JSON.stringify(result.response));
-                console.log('Result_' + result.response[0] + '_ending');
-                alert("success");
-                alert(JSON.stringify(result.response));
-
-            }, function(err) {
-                console.log("ERROR: " + JSON.stringify(err));
-                //alert(JSON.stringify(err));
-            }, function (progress) {
-                // constant progress updates
-            });
-
-
-        }, function(err) {
-            // error
-            console.log(err);
-        });
-    }
+    $scope.takePhoto = function () {
+		var options = {
+			quality: 75,
+			destinationType: Camera.DestinationType.DATA_URL,
+			sourceType: Camera.PictureSourceType.CAMERA,
+			allowEdit: true,
+			encodingType: Camera.EncodingType.JPEG,
+			targetWidth: 300,
+			targetHeight: 300,
+			popoverOptions: CameraPopoverOptions,
+			saveToPhotoAlbum: false
+		};
+		
+		$cordovaCamera.getPicture(options).then(function (imageData) {
+			$scope.imgURI = "data:image/jpeg;base64," + imageData;
+		}, function (err) {
+			// An error occured. Show a message to the user
+		});
+		}
+		
+		$scope.choosePhoto = function () {
+		var options = {
+			quality: 75,
+			destinationType: Camera.DestinationType.DATA_URL,
+			sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+			allowEdit: true,
+			encodingType: Camera.EncodingType.JPEG,
+			targetWidth: 300,
+			targetHeight: 300,
+			popoverOptions: CameraPopoverOptions,
+			saveToPhotoAlbum: false
+		};
+		
+		$cordovaCamera.getPicture(options).then(function (imageData) {
+				$scope.imgURI = "data:image/jpeg;base64," + imageData;
+			}, function (err) {
+				// An error occured. Show a message to the user
+			});
+		}
 })
 
 .controller('PlaylistsCtrl', function($scope) {
